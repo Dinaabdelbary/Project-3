d13e82f00f3a0d97164d29f73c39686ba6b986be
Jose Antonio Carmona <jacarmonamartin@gmail.com>
2022-05-05 19:57:23 +0200

    Add band model and update user model

--- bandapp-server/models/User.model.js
+++ bandapp-server/models/User.model.js
@@ -1,5 +1,5 @@
-
 const { Schema, model } = require("mongoose");
+const Band = require('./Band.model')
 
 const userSchema = Schema({
     email : {
@@ -15,15 +15,16 @@ const userSchema = Schema({
       type: String, //list of instruments and proficiency(not required)
       required: true
     },
-    personalchat : [{type: Schema.Types.ObjectId, ref:'Chat'}],
     instruments : [String],
-    location : String,
-    image : String,
-    listensto : [String],
+    location : String, //possible API call to location API, otherwise just city
+    imageUrl : String,
+    listensto : [String], //possible API call to Spotify
     genres : [String],
-    history : String,
-    connections : [String]
+    currentBands: [{type: Schema.Types.ObjectId, ref:Band}],
+    pendingRequests: [{type: Schema.Types.ObjectId, ref: "User"}],
+    successfulMatch: [{type: Schema.Types.ObjectId, ref: "User"}],
+    notifications: [{type: Schema.Types.Mixed, ref: "User"}]
 })
 
 
-module.exports = model("User",userSchema);
+module.exports = model("User", userSchema);
