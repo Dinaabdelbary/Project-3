d2461dd4daf37f241477c641df5ccfec3aea62a1
Dina Abdelbary <63638151+Dinaabdelbary@users.noreply.github.com>
2022-05-05 19:00:05 +0200

    fix usermodel

--- bandapp-server/configs/passport.js
+++ bandapp-server/configs/passport.js
@@ -1,4 +1,4 @@
-const User = require('../models/User.js');
+const User = require('../models/User.model');
 const LocalStrategy = require('passport-local').Strategy;
 const bcrypt = require('bcrypt');
 const passport = require('passport');
--- bandapp-server/routes/User.routes.js
+++ bandapp-server/routes/User.routes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const User = require('../models/User');
+const User = require('../models/User.model');
 
 
 // ===================================//Post /api/user ==== CREATE A USER  =============================
