a57d18dbc16ee15df45c2033f8fff06c8f398779
Dina Abdelbary <63638151+Dinaabdelbary@users.noreply.github.com>
2022-05-10 13:16:19 +0200

    profilepage

--- bandapp-server/client/src/App.js
+++ bandapp-server/client/src/App.js
@@ -18,7 +18,8 @@ import axios from 'axios';
 
 import UserProfile from './components/Geolocation';
 // import { ChakraProvider } from '@chakra-ui/react'
-import ProfileCard from './components/ProfileCard';
+// import ProfileCard from './components/ProfileCard';
+import ProfilePage from './components/ProfilePage';
 
 
 function App() 
@@ -74,6 +75,7 @@ function App()
           <Route path="/signup" element={<SignUp setLoggedInUser={setLoggedInUser}/>} />
           <Route path="/login" element={<Login setLoggedInUser={setLoggedInUser}/>} />
           <Route path="/location" element={<UserProfile />} />
+          <Route path="/:id" element={<ProfilePage/>} />       
         </Routes>
         {/* <div>
           <ProfileCard/>
--- bandapp-server/client/src/features/userApi/userApi.js
+++ bandapp-server/client/src/features/userApi/userApi.js
@@ -1,1 +1,15 @@
 import axios from "axios";
+
+
+const getuser = (id) => {
+    return axios
+        .get(`/api/user/${id}`)
+        .then(response => {
+            return (response.data) 
+        })
+        .catch(error => {
+            return error.response.data
+        });
+    }
+
+    export{ getuser }
