9a05dafff2cd9b1fb66ead9709954d04d51b6e65
j-jmnz <jmnz.jesus@gmail.com>
2022-05-13 12:43:39 +0200

    refactor/clean-up

--- bandapp-server/client/src/components/ListOfUsers.jsx
+++ bandapp-server/client/src/components/ListOfUsers.jsx
@@ -1,55 +1,61 @@
-import React, { Profiler, useEffect, useState } from 'react';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { storedUser } from '../features/auth/authSlice';
-import { getUserList } from '../features/userApi/userApi';
+import { getUserList } from '../services/userApi';
 import ProfileCard from './ProfileCard';
+
 const UsersList = () => {
-  const userData = useSelector(storedUser); // returns data from redux store
-  const [listOfUsers, setListOfUsers] = useState([]);
+    const userData = useSelector(storedUser); // returns data from redux store
+    const [listOfUsers, setListOfUsers] = useState([]);
 
-  const handleConnect = (id) => {
-    axios
-      .get(`/connect/${id}`)
-      .then()
-      .catch((err) => console.log(err));
-  };
- 
-  useEffect(() => {
-    getUserList()
-      .then((response) => {
-        const users = response.data;
-        setListOfUsers(users);
-      })
-      .catch((error) => console.log(error));
-  }, []);
+    const handleConnect = id => {
+        axios
+            .get(`/connect/${id}`)
+            .then()
+            .catch(err => console.log(err));
+    };
 
-  const allUsers = listOfUsers.map((user) => {
-    // console.log(userData.currentUser);
-      const isPending = userData.curerntUser?.pendingSentRequests.includes(user._id)
-    // console.log('isPending', isPending)
+    useEffect(() => {
+        getUserList()
+            .then(response => {
+                const users = response.data;
+                setListOfUsers(users);
+            })
+            .catch(error => console.log(error));
+    }, []);
+
+    const allUsers = listOfUsers.map(user => {
+        // console.log(userData.currentUser);
+        const isPending = userData.curerntUser?.pendingSentRequests.includes(
+            user._id
+        );
+        // console.log('isPending', isPending)
+        return (
+            <div key={user._id}>
+                <ProfileCard />
+                <Link to={`/${user._id}`}>{user.name}</Link>
+                <button
+                    className='raise'
+                    disabled={isPending}
+                    onClick={() => {
+                        handleConnect(user._id);
+                    }}
+                >
+                    Connect
+                </button>
+                )
+            </div>
+        );
+    });
+
     return (
-      <div key={user._id}>
-        
-        <ProfileCard />
-        <Link to={`/${user._id}`}>{user.name}</Link>
-          <button className="raise"
-            disabled={isPending}
-            onClick={() => {
-              handleConnect(user._id);
-            }}>Connect</button>
-        )
-      </div>
+        <div>
+            <h2>List of users</h2>
+            {allUsers}
+        </div>
     );
-  });
-
-  return (
-    <div>
-      <h2>List of users</h2>
-      {allUsers}
-    </div>
-  );
 };
 
 export default UsersList;
--- bandapp-server/client/src/components/ProfileCard.jsx
+++ bandapp-server/client/src/components/ProfileCard.jsx
@@ -1,44 +1,58 @@
-import React, { useState, useEffect } from 'react';
-import { getUser } from '../features/userApi/userApi';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { Link } from 'react-router-dom';
-function ProfileCard() {
-  const [user, setUser] = useState({
-    name: '',
-    instruments: [],
-    location: '',
-    profilePicture: '',
-    genres: [],
-  });
-  const { id } = useParams();
-  useEffect(() => {
-    setUser(getUser(id));
-  }, []);
- 
-  const clickHandler = () => {
-    console.log('clicked');
-  };
-  return (
-    <div className='profileList'>
-    <div className='profileCard raise'>
-    <div class="card-header">
-      <div class="card-header-slanted-edge">
-      <img>{user.img}</img>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 200"><path class="polygon" d="M-20,200,1000,0V200Z" /></svg>  
-      </div>
-      </div>
-      <div class="card-body">
-      <h2 className="cardname">{user.name}name</h2>
-      <h4 class="title">Guitarist</h4>
-      <div class="bio">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos, aperiam.</div>
-      <p className="details">{user.instruments}instruments</p>
-      <p className="details">{user.location}location</p>
-      <button className="button" onClick={clickHandler()}>add friend</button>
-      <button className="button" onClick={clickHandler()}>chat</button>
-     </div>
-    </div>
-    </div>
-  );
-}
+import { getUser } from '../services/userApi';
+
+const ProfileCard = () => {
+    const [user, setUser] = useState({
+        name: '',
+        instruments: [],
+        location: '',
+        profilePicture: '',
+        genres: [],
+    });
+    const { id } = useParams();
+
+    useEffect(() => {
+        setUser(getUser(id));
+    }, []);
+
+    const clickHandler = () => {
+        console.log('clicked');
+    };
+
+    return (
+        <div className='profileList'>
+            <div className='profileCard raise'>
+                <div class='card-header'>
+                    <div class='card-header-slanted-edge'>
+                        <img>{user.img}</img>
+                        <svg
+                            xmlns='http://www.w3.org/2000/svg'
+                            viewBox='0 0 1000 200'
+                        >
+                            <path class='polygon' d='M-20,200,1000,0V200Z' />
+                        </svg>
+                    </div>
+                </div>
+                <div class='card-body'>
+                    <h2 className='cardname'>{user.name}name</h2>
+                    <h4 class='title'>Guitarist</h4>
+                    <div class='bio'>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit. Dignissimos, aperiam.
+                    </div>
+                    <p className='details'>{user.instruments}instruments</p>
+                    <p className='details'>{user.location}location</p>
+                    <button className='button' onClick={clickHandler()}>
+                        add friend
+                    </button>
+                    <button className='button' onClick={clickHandler()}>
+                        chat
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+};
 
 export default ProfileCard;
