48dc39e49d7baa56f7ae8a550b0083e5ba33775f
Dina Abdelbary <63638151+Dinaabdelbary@users.noreply.github.com>
2022-05-14 16:14:34 +0200

    socket-io-update

--- bandapp-server/client/src/components/Chat.jsx
+++ bandapp-server/client/src/components/Chat.jsx
@@ -1,8 +1,30 @@
+import { compileClientWithDependenciesTracked } from 'jade';
 import React from 'react'
 
 function Chat(props) {
-  return (
-    <div id='chat-container'>Chat</div>
+  const [feed, setFeed] = React.useState([]);
+  const socketRef = React.useRef();
+
+React.useEffect(() => {
+  chat.getPreviousMessages(room).then(res => setFeed(res.data)).catch(err => console.log(err))
+}, []);
+
+React.useEffect(() => {
+  socketRef.current = io.connect(process.env.REACT_APP_API_BASE_URL);
+  socketRef.current.on('message', (messageData) => {
+    setFeed([...feed, messageData])
+  });
+  return () => socketRef.current.disconnet();
+},[feed]);
+
+const handleSendMessage = (nesMessage) => {
+  chat.sendMessage(LoggedInUser, room, newMessage).then(response => {
+    socketRef.current.emit('message', { ...response.data, room, sendBy: LoggedInUser})
+    setFeed([...feed, { ...response.data, senBy: LoggedInUser}])
+  }).catch(err => console.log(err))
+}
+return (
+  <div id='chat-container'>Chat</div>
   )
 }
 
