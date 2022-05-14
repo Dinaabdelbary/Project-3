1da6ffe3aee69e83f4f0634ccc9429e1f1a8ce0d
Jose Antonio Carmona <jacarmonamartin@gmail.com>
2022-05-14 15:24:15 +0200

    display search result

--- bandapp-server/client/src/components/SearchResult.jsx
+++ bandapp-server/client/src/components/SearchResult.jsx
@@ -1,23 +1,28 @@
 //Display response.data, which is an array with the results of the search
 
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useLocation } from 'react-router-dom'
 import getSearchResult from '../services/searchApi'
+import ProfileCard from './ProfileCard'
 
 const SearchResult = () => {
+
+    const [userList, setUserList] = useState([])
 
     const query = useLocation().search
     useEffect(()=>{
         getSearchResult(query)
     .then((response) => {
         const allresults = response.data
+        setUserList(allresults)
     }).catch(error => console.log(error))
-    })
+    }, [])
     
     return (
     <div>
-
-
+    {userList.map((user) => {
+        return <ProfileCard user={user}/>
+    })}
     </div>
   )
 }
