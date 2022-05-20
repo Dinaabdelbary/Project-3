8937618f4509b0edf60842ef056e4522cb98290e
Jose Antonio Carmona <jacarmonamartin@gmail.com>
2022-05-20 16:20:13 +0200

    Delete client

--- client/src/services/locationApi.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const getLocation = () => {
-  return axios.get("https://ipinfo.io/json?token=f0585fac666470");
-};
-
-export { getLocation };
--- client/src/services/searchApi.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const getSearchResults = (query) => {
-    return axios.get(`http://localhost:3005/api/search?q=${query}`)
-}
-
-export default getSearchResults
