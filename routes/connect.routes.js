c269df4e95570cec36b8e1b0f9006f5ac978660e
Jose Antonio Carmona <jacarmonamartin@gmail.com>
2022-05-20 21:46:07 +0200

    connect routes fix by vitor

--- routes/connect.routes.js
+++ routes/connect.routes.js
@@ -1,9 +1,10 @@
 const User = require('../models/User.model');
 const router = require('express').Router();
 
-router.get('/accept/:id', (req, res) => {
+router.post('/accept/:id', (req, res) => {
   const otherUserId = req.params.id;
-  const currentUserId = req.session.passport.user;
+  const { currentUserId } = req.body;
+
 
   User.findOneAndUpdate(
     { _id: currentUserId },
@@ -30,11 +31,14 @@ router.get('/accept/:id', (req, res) => {
     .catch((err) => console.log(err));
 });
 
-router.get('/decline/:id', (req, res) => {
+
+// CHANGED
+router.post('/decline/:id', (req, res) => {
   const otherUserId = req.params.id;
-  console.log('req.params.id: ', otherUserId);
-  const currentUserId = req.session.passport.user;
-  console.log('req.session.passport.user: ', currentUserId);
+
+  const { currentUserId } = req.body;
+
+
 
   User.findOneAndUpdate(
     { _id: currentUserId },
@@ -84,37 +88,38 @@ router.get('/decline/:id', (req, res) => {
 //     .catch((err) => console.log(err));
 // });
 
-router.get('/:id', (req, res) => {
+ // CHANGED
+router.post('/:id', (req, res) => {
   const otherUserId = req.params.id;
-  const currentUserId = req.session.passport.user;
+  const { currentUserId } = req.body;
 
   User.findOneAndUpdate(
     { _id: currentUserId },
     { $push: { pendingSentRequests: otherUserId } },
     { new: true }
-  ).populate('pendingSentRequests')
+  )
+    .populate('pendingSentRequests')
     .then((updatedUser) => {
       req.session.currentUser = updatedUser;
       User.findOneAndUpdate(
         { _id: otherUserId },
         { $push: { pendingReceivedRequests: currentUserId } },
         { new: true }
-      ).populate('pendingReceivedRequests')
+      )
+        .populate('pendingReceivedRequests')
         .then(() => console.log('updated added user'))
-        .catch((error) =>
-          console.error(error, 'error when updating added user')
-        );
+        .catch((error) => console.log(error, 'error when updating added user'));
       res.json(updatedUser);
     })
     .catch((err) =>
-      console.log(err, error, 'error when updating request sender user')
+      console.log(err, 'error when updating request sender user')
     );
 });
 
-//unfollow
-router.get('/unfollow/:id', (req, res) => {
+//unfollow CHANGED
+router.post('/unfollow/:id', (req, res) => {
   const otherUserId = req.params.id;
-  const currentUserId = req.session.passport.user;
+  const { currentUserId } = req.body;
 
   User.findOneAndUpdate(
     { _id: currentUserId },
